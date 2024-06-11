60d99041960bc6d0f81af6a5c62d2dd2cabffcaf
solaudeen <solaudeenw@gmail.com>
2024-06-10 23:02:43 -0700

    completed create page UI form

---
+++ src/data/blogCategories.js
@@ -0,0 +1,105 @@
+export const blogCategories = [
+  {
+    category: "Technology",
+    subCategories: [
+      "Programming",
+      "Web Development",
+      "Gadgets",
+      "Software",
+      "IT",
+    ],
+  },
+
+  {
+    category: "Lifestyle",
+    subCategories: [
+      "Health and Wellness",
+      "Fitness",
+      "Travel",
+      "Food and Drink",
+      "Personal Finance",
+    ],
+  },
+
+  {
+    category: "Business",
+    subCategories: [
+      "Marketing",
+      "Enterpreneurship",
+      "Management",
+      "Leadership",
+      "Industry news",
+    ],
+  },
+
+  {
+    category: "Entertainment",
+    subCategories: ["Movies", "Music", "TV Shows", "Books", "Celebrity news"],
+  },
+
+  {
+    category: "Education",
+    subCategories: [
+      "Study tips",
+      "Learning resources",
+      "Industry insights",
+      "Career development",
+      "Academic research",
+    ],
+  },
+
+  {
+    category: "Personal growth",
+    subCategories: [
+      "Self-improvement",
+      "Productivity",
+      "Motivation",
+      "Mindfulness",
+      "Goal setting",
+    ],
+  },
+
+  {
+    category: "News and current events",
+    subCategories: [
+      "Politics",
+      "Social justice",
+      "Environmental issues",
+      "Economic news",
+      "Science and research",
+    ],
+  },
+
+  {
+    category: "Arts and culture",
+    subCategories: [
+      "Arts",
+      "Design",
+      "Fashion",
+      "Photography",
+      "Culture and society",
+    ],
+  },
+
+  {
+    category: "Home and garden",
+    subCategories: [
+      "DIY projects",
+      "Home decor",
+      "Gardening",
+      "Cooking",
+      "Home improvement",
+    ],
+  },
+
+  {
+    category: "Sports and fitness",
+    subCategories: [
+      "Workout routines",
+      "Sports news",
+      "Athlete interviews",
+      "Fitness tips",
+      "Healthy living",
+    ],
+  },
+];
--- src/routes/CreateBlog.jsx
+++ src/routes/CreateBlog.jsx
@@ -1,106 +1,122 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Input from "../components/Input";
+import { blogCategories } from "../data/blogCategories";
+import { toast } from "react-toastify";
 
 const CreateBlog = () => {
+  const publishDateRef = useRef();
+  const statusRef = useRef();
+  const blogTitleRef = useRef();
+  const metaDescriptionRef = useRef();
+  const categoryRef = useRef();
+  const subCategoryRef = useRef();
+  const contentFieldRef = useRef();
+  const thumbNailRef = useRef();
+
+  //function to check if input consists of only spaces
+  const isOnlySpaces = (value) => {
+    return value.trim(" ").length === 0;
+  };
+
+  const hanldeFormSubmit = (e) => {
+    e.preventDefault();
+
+    const date = publishDateRef.current.innerHTML;
+    const status = statusRef.current.value;
+    const Title = blogTitleRef.current.value;
+    const Description = metaDescriptionRef.current.value;
+    const category = categoryRef.current.value;
+    const subCategory = subCategoryRef.current.value;
+    const content = contentFieldRef.current.value;
+    const thumbnail = thumbNailRef.current.files[0];
+
+    //check if title is empty contain only spaces
+    if (isOnlySpaces(Title)) {
+      toast.error("Title space cannot be empty");
+      return;
+    }
+
+    //check if description is empty contain only spaces
+    if (isOnlySpaces(Description)) {
+      toast.error("Summarize your content");
+      return;
+    }
+
+    //check if content is empty contain only spaces
+    if (isOnlySpaces(content)) {
+      toast.error("Write your content ");
+      return;
+    }
+
+    const blogData = {
+      blogDate: date,
+      blogStatus: status,
+      blogTitle: Title,
+      BlogDescription: Description,
+      BlogCategory: category,
+      BlogSubCategory: subCategory,
+      BlogContent: content,
+      BlogThumbnail: thumbnail,
+    };
+
+    console.log(blogData);
+  };
   return (
     <section className="max-w-2xl mx-auto mb-10">
-      <form className="w-[95%] mx-auto rounded-lg shadow-lg px-4 mt-6 py-4 bg-gray-50">
+      <form
+        className="w-[95%] mx-auto rounded-lg shadow-lg px-4 mt-6 py-4 bg-gray-50"
+        onSubmit={hanldeFormSubmit}
+      >
         <h2 className="text-2xl font-bold text-gray-800">Create A Blog</h2>
 
         <div className="mt-6 flex items-center justify-between flex-wrap">
-          <p className="block mb-2 text-sm font-medium text-gray-900">
-            Publish Date: <span className="text-gray-400"> 12th june 2024</span>
-          </p>
+          <PublishDate reference={publishDateRef} />
 
           {/* status of blog */}
-          <div className="max-w-40 flex items-center gap-2">
-            <label
-              htmlFor={"statu"}
-              className="block text-sm font-medium text-gray-900"
-            >
-              Status:
-            </label>
-            <input
-              type={"text"}
-              name={"Status"}
-              id={"Blog-Status"}
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-1"
-            />
-            <div
-              id="standard_error_help"
-              className="mt-2 text-xs text-red-600 dark:text-red-400"
-            ></div>
-          </div>
+          <StatusInput reference={statusRef} />
         </div>
 
         <div className="mt-6 flex flex-wrap gap-6">
           {/* title input */}
           <div className="w-full max-w-64">
-            <Input
+            <BlogInput
               htmlFor={"Title"}
               label={"Title"}
               type={"Text"}
               name={"Title"}
               id={"Blog-Title"}
               placeholder={"Blog title"}
-              onChange={() => console.log("changing")}
-              value={"20"}
-            />
-          </div>
-          {/* category input */}
-          <div className="w-full max-w-64">
-            <Input
-              htmlFor={"Category"}
-              label={"Category"}
-              type={"Text"}
-              name={"Category"}
-              id={"Blog-Category"}
-              placeholder={"Blog Category"}
-              onChange={() => console.log("changing")}
-              value={"20"}
-            />
-          </div>
-          {/* subcategory input */}
-          <div className="w-full max-w-64">
-            <Input
-              htmlFor={"Subcategory"}
-              label={"Subcategory"}
-              type={"Text"}
-              name={"Subcategory"}
-              id={"Blog-Subcategory"}
-              placeholder={"Blog Subcategory"}
-              onChange={() => console.log("changing")}
-              value={"20"}
+              reference={blogTitleRef}
             />
           </div>
 
           {/* Meta Description input */}
           <div className="w-full max-w-64">
-            <Input
-              htmlFor={"MetaDescription"}
-              label={"Meta Description"}
-              type={"Text"}
-              name={"MetaDescription"}
-              id={"Meta-Description"}
-              placeholder={"summarize your blog in 200 words"}
-              onChange={() => console.log("changing")}
-            />
+            <MetaDescription reference={metaDescriptionRef} />
           </div>
+        </div>
+
+        {/* category and subcategories input */}
+        <div className="mt-6">
+          <Categories referenceA={categoryRef} referenceB={subCategoryRef} />
         </div>
 
         {/* content area */}
         <div className="mt-6">
-          <TextArea />
+          <TextArea reference={contentFieldRef} />
         </div>
 
         {/* file Input */}
         <div className="mt-6">
-          <FileInput />
+          <ThumbNail reference={thumbNailRef} />
         </div>
 
         {/* post blog button */}
         <div className="flex justify-end">
-          <button className="inline-flex items-center mt-8 py-2.5 px-4 text-xs font-medium text-center text-white bg-gray-700 rounded-lg">
+          <button
+            type="submit"
+            className="inline-flex items-center mt-8 py-2.5 px-4 text-xs font-medium text-center text-white bg-gray-700 rounded-lg"
+          >
             Post blog
           </button>
         </div>
@@ -111,57 +127,321 @@ const CreateBlog = () => {
 
 export default CreateBlog;
 
-export const TextArea = () => {
+//text are input
+export const TextArea = ({ reference }) => {
+  const [value, setValue] = useState("");
+  const handleOnChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
-    <>
+    <div>
       <label
-        for="message"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        htmlFor="blog-content"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       >
         Content
       </label>
       <textarea
-        id="message"
-        rows="4"
+        id="blog-content"
+        ref={reference}
+        rows="6"
+        value={value}
+        onChange={handleOnChange}
+        maxLength={5000}
         className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Write your content here..."
+        placeholder="What do you want to talk about?"
       ></textarea>
+      <p className="mt-2 ms-auto text-xs text-gray-500 dark:text-gray-400">
+        {value.length >= 5000
+          ? "You have reached the limit"
+          : `You have ${5000 - value.length} words left`}
+      </p>
+    </div>
+  );
+};
+
+//file input
+export const ThumbNail = ({ reference }) => {
+  const [url, seturl] = useState(null);
+  console.log(url);
+  const handleOnChange = (e) => {
+    seturl(URL.createObjectURL(e.target.files[0]));
+  };
+  return (
+    <div>
+      <div className={`flex items-center justify-center w-full`}>
+        <label
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          for="multiple_files"
+        ></label>
+        <input
+          className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          id="image"
+          type="file"
+          accept="jpg, jpeg, png, svg"
+          ref={reference}
+          onChange={handleOnChange}
+        />
+      </div>
+
+      {url && (
+        <div>
+          <p className="mt-4 ms-auto text-xs text-gray-500 dark:text-gray-400">
+            This image will be used as thumbnail
+          </p>
+          <img src={url} alt="" className="w-full object-contain mt-2" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+//publish date
+export const PublishDate = ({ reference }) => {
+  // Create a new Date object for the current date and time
+  const today = new Date();
+
+  // Extract the year, month, and day
+  const year = today.getFullYear();
+  const monthIndex = today.getMonth(); // Months are zero-indexed (0-11)
+  const day = today.getDate();
+
+  // Array of month names
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  // Get the name of the month
+  const monthName = monthNames[monthIndex];
+
+  // Format the date as Month Day, Year
+  const formattedDate = `${monthName} ${day}, ${year}`;
+  return (
+    <>
+      <p className="block text-sm font-medium text-gray-900">
+        Publish Date:{" "}
+        <span className="text-gray-400" ref={reference}>
+          {" "}
+          {formattedDate}
+        </span>
+      </p>
     </>
   );
 };
 
-export const FileInput = () => {
+//status input
+export const StatusInput = ({ reference }) => {
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="max-w-40 flex items-center gap-2">
       <label
-        htmlFor="dropzone-file"
-        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        htmlFor={"status"}
+        className="block text-sm font-medium text-gray-900"
       >
-        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg
-            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 16"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-            />
-          </svg>
-          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-            <span className="font-semibold">Click to upload</span>
-          </p>
-          <p className="text-xs text-gray-500 dark:text-gray-400">
-            SVG, PNG or JPG (MAX. 800x400px)
-          </p>
-        </div>
-        <input id="dropzone-file" type="file" className="hidden" />
+        Status:
       </label>
+      <select
+        id="Blog-Status"
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-1"
+        ref={reference}
+      >
+        <option defaultValue="publish">Publish</option>
+        <option value="draft">Draft</option>
+      </select>
+    </div>
+  );
+};
+
+//create blog input for title
+export const BlogInput = ({
+  htmlFor,
+  label,
+  type,
+  name,
+  id,
+  placeholder,
+  reference,
+}) => {
+  const [value, setValue] = useState("");
+  const handleOnChange = (e) => {
+    setValue(e.target.value);
+  };
+  return (
+    <div>
+      <label
+        htmlFor={htmlFor}
+        className="block mb-2 text-sm font-medium text-gray-900"
+      >
+        {label}
+      </label>
+      <input
+        type={type}
+        name={name}
+        id={id}
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2"
+        placeholder={placeholder}
+        onChange={handleOnChange}
+        value={value}
+        ref={reference}
+      />
+      <div
+        id="standard_error_help"
+        className="mt-2 text-xs text-red-600 dark:text-red-400"
+      ></div>
+    </div>
+  );
+};
+
+//categories
+export const Categories = ({ referenceA, referenceB }) => {
+  const [value, setValue] = useState("Technology");
+  const [subCategoryValue, setSubCategoryValue] = useState(
+    blogCategories.find((category) => category.category === value)
+      .subCategories[0]
+  );
+
+  const [showCategoryModal, setShowCategoryModal] = useState(false);
+  const [showSubCategoryModal, setShowSubCategoryModal] = useState(false);
+
+  return (
+    <div className="flex gap-6 items-center flex-wrap">
+      {/* category */}
+      <div id="category" className="relative w-full max-w-64">
+        <div className="w-full" onClick={() => setShowCategoryModal(true)}>
+          <label
+            htmlFor={"Categories"}
+            className="block mb-2 text-sm font-medium text-gray-900 "
+          >
+            Category
+          </label>
+          <input
+            type={"text"}
+            name={"Category"}
+            id={"Category"}
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full py-2 px-3 cursor-pointer"
+            readOnly
+            value={value}
+            ref={referenceA}
+          />
+          <div
+            id="standard_error_help"
+            className="mt-2 text-xs text-red-600 dark:text-red-400"
+          ></div>
+        </div>
+
+        {/*category modal */}
+        {showCategoryModal && (
+          <ul className="absolute z-10 right-0 left-0 space-y-1 bg-white h-40 rounded-lg overflow-y-auto shadow-md py-2">
+            {blogCategories.map((category) =>
+              value === category.category ? null : (
+                <li
+                  className="px-3 cursor-pointer py-1 hover:bg-gray-50 text-gray-600 hover:text-gray-800"
+                  key={category.category}
+                  onClick={() => {
+                    setValue(category.category);
+                    setSubCategoryValue(
+                      blogCategories.find(
+                        (blogcategory) =>
+                          blogcategory.category === category.category
+                      ).subCategories[0]
+                    );
+                    setShowCategoryModal(false);
+                  }}
+                >
+                  {category.category}
+                </li>
+              )
+            )}
+          </ul>
+        )}
+      </div>
+
+      {/* sub category */}
+      <div id="category" className="relative w-full max-w-64">
+        <div className="w-full" onClick={() => setShowSubCategoryModal(true)}>
+          <label
+            htmlFor={"Categories"}
+            className="block mb-2 text-sm font-medium text-gray-900 "
+          >
+            Subcategory
+          </label>
+          <input
+            type={"text"}
+            name={"subCategory"}
+            id={"subCategory"}
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full py-2 px-3 cursor-pointer"
+            readOnly
+            value={subCategoryValue}
+            ref={referenceB}
+          />
+          <div
+            id="standard_error_help"
+            className="mt-2 text-xs text-red-600 dark:text-red-400"
+          ></div>
+        </div>
+
+        {/*subcategory modal */}
+        {showSubCategoryModal && (
+          <ul className="absolute right-0 left-0 space-y-1 bg-white h-40 rounded-lg overflow-y-auto shadow-md py-2">
+            {blogCategories
+              .find((category) => category.category === value)
+              .subCategories.map((subCategory) =>
+                subCategoryValue === subCategory ? null : (
+                  <li
+                    className="px-3 cursor-pointer py-1 hover:bg-gray-50 text-gray-600 hover:text-gray-800"
+                    key={subCategory}
+                    onClick={() => {
+                      setSubCategoryValue(subCategory);
+                      setShowSubCategoryModal(false);
+                    }}
+                  >
+                    {subCategory}
+                  </li>
+                )
+              )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export const MetaDescription = ({ reference }) => {
+  const [value, setValue] = useState("");
+  const handleOnChange = (e) => {
+    setValue(e.target.value);
+  };
+  return (
+    <div>
+      <label
+        htmlFor="meta-description"
+        className="block mb-2 text-sm font-medium text-gray-900"
+      >
+        Meta description
+      </label>
+      <input
+        type="text"
+        name="meta-description"
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2"
+        placeholder="Sumarise in 1000 words"
+        onChange={handleOnChange}
+        value={value}
+        ref={reference}
+      />
+      <div
+        id="standard_error_help"
+        className="mt-2 text-xs text-red-600 dark:text-red-400"
+      ></div>
     </div>
   );
 };
